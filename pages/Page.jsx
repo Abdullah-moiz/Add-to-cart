a3573e6a33f4e3cbfb092e2e24a77e3f85423d19
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-03-24 12:23:08 +0500

    Add to Cart Done

--- pages/Page.jsx
+++ pages/Page.jsx
@@ -3,44 +3,76 @@ import React, { useEffect, useState } from 'react'
 import { ToastContainer } from 'react-toastify';
 import { BsCart } from 'react-icons/bs'
 import data from '../api/index';
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 import { AiFillDelete } from 'react-icons/ai'
 import { useDispatch, useSelector } from 'react-redux';
-import { setCart, setDeleteCart } from '@/slices/cartSlice';
+import { setDeleteCart } from '@/slices/cartSlice';
 
 
 export default function Landing() {
     const dispatch = useDispatch();
-    const router = useRouter();
+
+    
     const [product, setProduct] = useState([]);
-    const [qnt, setQnt] = useState(1)
+    const [qnt, setQnt] = useState(0)
+
+
 
     const cart = useSelector(state => state.cart.cart);
+
 
 
     useEffect(() => {
         setProduct(data)
     }, [])
 
-    const handleIncrement = () => {
+    const handleIncrement = (id) => {
+        const updatedCartItems = cart?.map(prod => {
+            if (prod.id === id) {
+                let newQuantity =  prod?.product_quantity +  1
+                return {
+                    ...prod,
+                    product_quantity: newQuantity ,
+                };
+            }
+            return prod;
+        });
+
         if (qnt > 0) {
             let quantity = qnt + 1
             setQnt(quantity)
+            dispatch(setDeleteCart(updatedCartItems))
         }
         else {
             setQnt(1)
+            dispatch(setDeleteCart(updatedCartItems))
         }
     }
 
-    const handleDecrement = () => {
+
+
+    const handleDecrement = (id) => {
+        const updatedCartItems = cart?.map(prod => {
+            if (prod.id === id) {
+                let newQuantity =  prod?.product_quantity -  1
+                return {
+                    ...prod,
+                    product_quantity: newQuantity ,
+                };
+            }
+            return prod;
+        });
+
         if (qnt > 1) {
             let quantity = qnt - 1
             setQnt(quantity)
+            dispatch(setDeleteCart(updatedCartItems))
         }
         else {
             setQnt(1)
+            dispatch(setDeleteCart(updatedCartItems))
+
         }
     }
 
@@ -49,14 +81,20 @@ export default function Landing() {
         dispatch(setDeleteCart(newCart))
     }
 
+    const totalPrice = cart.reduce((acc, item) => {
+        return acc + (item.product_quantity * item.product_price);
+      }, 0);
+
 
 
     return (
         <div className='w-full px-2 py-2 flex flex-col items-center'>
             <h1 className='text-lg font-extrabold py-2 mb-2 border-b border-indigo-600'>Shopping Cart</h1>
-            <Link href="#cart" className='relative'><BsCart className="fixed md:text-6xl mx-4 top-2 right-0 text-2xl" /></Link>
-            <p className='absolute z-10 top-0 right-3 text-red-600 text-xl font-semibold'>{cart.length}</p>
-            <div className='border w-full h-full flex md:flex-row flex-col '>
+            <div className='fixed top-0 right-0 '>
+                <Link href="#cart" className='relative'><BsCart className=" md:text-6xl mx-4  text-2xl" /></Link>
+                <p className='absolute z-10 top-0 right-1 text-red-600 text-xl font-semibold'>{cart.length}</p>
+            </div>
+            <div className=' w-full h-full flex md:flex-row flex-col '>
                 <div className=':w-8/12 w-full py-4 flex flex-wrap'>
                     {
 
@@ -65,7 +103,7 @@ export default function Landing() {
                         })
                     }
                 </div>
-                <div id='cart' className='lg:w-4/12 w-full'>
+                <div id='cart' className='lg:w-4/12 w-full py-4 px-2'>
                     {
                         cart?.map((prod) => {
                             return (
@@ -74,9 +112,9 @@ export default function Landing() {
                                     <p>{prod?.product_name}</p>
                                     <p>{prod?.product_price}</p>
                                     <div className='flex '>
-                                        <button onClick={handleIncrement} className="cursor-pointer text-xl">+</button>
-                                        <p className='text-xl'>{qnt}</p>
-                                        <button onClick={handleDecrement} className="cursor-pointer text-xl">-</button>
+                                        <button onClick={() => handleIncrement(prod?.id)} className="cursor-pointer text-xl">+</button>
+                                        <p className='text-xl'>{prod?.product_quantity}</p>
+                                        <button onClick={() => handleDecrement(prod?.id)} className="cursor-pointer text-xl">-</button>
                                     </div>
                                     <AiFillDelete onClick={() => handleDelete(prod?.id)} className="text-red-500 cursor-pointer text-xl" />
 
@@ -85,6 +123,9 @@ export default function Landing() {
                         })
                     }
 
+                    <div className='px-2 py-2 flex justify-end items-center'>
+                        <p>Total Price {totalPrice}</p>
+                    </div>
                 </div>
             </div>
             <ToastContainer />
